8d028b47bc52533cf82abd3b23eb97f6064f4bfe
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2021-01-23 22:14:01 +0530

    pagination

--- Antikual/Resources/driveAPI.py
+++ Antikual/Resources/driveAPI.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse, request
 from flask import Response
-import os, json, time, uuid
+import os, json, time, base64
 from flask_limiter.util import get_remote_address
 from Antikual import limiter
 
@@ -20,13 +20,18 @@ class getFiles(Resource):
     ]
     def get(self):
         file_name = request.args.get('name','').strip()
+        page_token = request.args.get('token',None).strip()
+        order_by = request.args.get('orderBy','name').strip()
+        if(page_token != None):
+            page_token = base64.b64decode(page_token).decode()
         #if(len(file_name) < 3):
          #   return Response(json.dumps({'message': 'Send atleast 3 initials'}), status=403, mimetype='application/json')
         fields_to_get = 'id,name,mimeType,webViewLink,thumbnailLink,owners(displayName,emailAddress)'
         files_resp = drive_service.files().list(
             pageSize=10,
-            orderBy='name',
+            orderBy=order_by,
             q = f"name contains '{file_name}'",
-            fields=f'nextPageToken, files({fields_to_get})'
+            fields=f'nextPageToken, files({fields_to_get})',
+            pageToken=page_token
             ).execute()
         return files_resp
